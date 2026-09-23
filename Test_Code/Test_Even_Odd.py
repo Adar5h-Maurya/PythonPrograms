3e27718b0bc886045bb9cf6dc4a0cd48e051b28d
Adar5h-Maurya <adarsh2608maurya@gmail.com>
2026-09-23 17:51:10 +0530

    Even_Odd Program Test Case

---
+++ Test_Code/Test_Even_Odd.py
@@ -0,0 +1,16 @@
+from Code.Even_Odd import check_even_odd
+
+assert check_even_odd(11) == "Odd"
+print("Test Case 1 pass")
+
+assert check_even_odd(20) == "Even"
+print("Test Case 2 pass")
+
+assert check_even_odd(27) == "Odd"
+print("Test Case 3 pass")
+
+assert check_even_odd(50) == "Even"
+print("Test Case 4 pass")
+
+assert check_even_odd(81) == "Odd"
+print("Test Case 5 pass")
