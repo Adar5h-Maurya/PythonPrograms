1bc1cbfcc23e414d37bc1640b37eeb81849c4280
Adar5h-Maurya <adarsh2608maurya@gmail.com>
2026-09-24 16:52:36 +0530

    Largest of Three no. Program

---
+++ Code/Largest_of_three.py
@@ -0,0 +1,10 @@
+def largest_of_three(a,b,c):
+    if a >= b and a >= c:
+        return a 
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+# print(largest_of_three(10,5,3))
+# print(largest_of_three(4,15,8))
