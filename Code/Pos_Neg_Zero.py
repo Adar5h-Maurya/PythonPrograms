81a32317f9a1007d22d43d4ddf227802efe045dd
Adar5h-Maurya <adarsh2608maurya@gmail.com>
2026-09-24 17:12:38 +0530

    No. Is positive_negative_zero Program

---
+++ Code/Pos_Neg_Zero.py
@@ -0,0 +1,11 @@
+def positive_negative_zero(num):
+    if num > 0:
+        return "Positive"
+    elif num < 0:
+        return "Negative"
+    else:
+        return "zero"
+
+print(positive_negative_zero(10))
+print(positive_negative_zero(-20))
+print(positive_negative_zero(0))
