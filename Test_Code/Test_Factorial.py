4ca5e4603b74b50939fc66c1f20bbb35157a1889
Adar5h-Maurya <adarsh2608maurya@gmail.com>
2026-09-24 17:39:15 +0530

    Factorial Program Test Case

---
+++ Test_Code/Test_Factorial.py
@@ -0,0 +1,16 @@
+from Code.Factorial import factorial
+
+assert factorial(2) == 2
+print("Test Case 1 pass")
+
+assert factorial(5) == 120
+print("Test Case 2 pass")
+
+assert factorial(0) == 1
+print("Test Case 3 pass")
+
+assert factorial(6) == 720
+print("Test Case 4 pass")
+
+assert factorial(7) == 5040
+print("Test Case 5 pass")
