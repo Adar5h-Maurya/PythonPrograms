9ec0512be9732e13bc338f487b950347a415c43d
Adar5h-Maurya <adarsh2608maurya@gmail.com>
2026-09-24 16:55:57 +0530

    Largest of Three no. program Test Case

---
+++ Test_Code/Test_Largest_of_three.py
@@ -0,0 +1,16 @@
+from Code.Largest_of_three import largest_of_three
+
+assert largest_of_three(10, 5, 3) == 10
+print("Test Case 1 pass")
+
+assert largest_of_three(4, 15, 8) == 15
+print("Test Case 2 pass")
+
+assert largest_of_three(2, 7, 20) == 20
+print("Test Case 3 pass")
+
+assert largest_of_three(50, 30, 40) == 50
+print("Test Case 4 pass")
+
+assert largest_of_three(12, 25, 18) == 25
+print("Test Case 5 pass")
