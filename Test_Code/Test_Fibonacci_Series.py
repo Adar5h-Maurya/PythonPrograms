0e7e84190d4399170bde655b3430611f99b4645d
Adar5h-Maurya <adarsh2608maurya@gmail.com>
2026-09-24 18:05:41 +0530

    Fibonacci Series Program Test Case

---
+++ Test_Code/Test_Fibonacci_Series.py
@@ -0,0 +1,16 @@
+from Code.Fibonacci_Series import fibonacci
+
+assert fibonacci(5) == [0, 1, 1, 2, 3]
+print("Test Case 1 pass")
+
+assert fibonacci(6) == [0, 1, 1, 2, 3, 5]
+print("Test Case 2 pass")
+
+assert fibonacci(3) == [0, 1, 1]
+print("Test Case 3 pass")
+
+assert fibonacci(1) == [0]
+print("Test Case 4 pass")
+
+assert fibonacci(7) == [0, 1, 1, 2, 3, 5, 8]
+print("Test Case 5 pass")
